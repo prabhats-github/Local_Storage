65226ce42c21bcf28001b986e9bfcd15919f793c
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-01-10 12:20:50 +0530

    Add files via upload

---
+++ localstorage_01.js
@@ -0,0 +1,36 @@
+// Save Data to Local Storage...
+// localStorage.setItem(key, value);
+
+// let obj = {
+//     "First_Name": "John",
+//     "Last_Name":"Murphy"
+// }
+
+localStorage.setItem("First_Name","john")
+localStorage.setItem("Last_Name","Murphy")
+
+
+
+// Read Data from Local Storage...
+// let a = localStorage.getItem(key);
+
+let a=localStorage.getItem('First_Name')
+console.log(a)
+
+let ln=localStorage.getItem('Last_Name')
+console.log(ln)
+
+// Remove Data from Local Storage...
+// localStorage.removeItem(key);
+
+let b=localStorage.removeItem('First_Name')
+console.log(a)
+console.log(b)
+console.log(ln)
+
+let clr=localStorage.clear()
+console.log(clr)
+
+
+
+
